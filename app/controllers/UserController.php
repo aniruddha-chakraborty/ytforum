c460e9bd44e0e034026e4ec5550080a84f3f4b65
appanik <project.anik@gmail.com>
2015-08-04 01:22:36 +0600

    FIrst commit

---
+++ app/controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+/**
+* 
+*/
+Class UserController extends BaseController {
+	
+		//get the view page for register
+		public function getCreate(){
+			
+				echo  "asni";
+		}
+
+		//get the view page for login
+
+		public function getLogin(){
+
+
+		}
+	
+}
+
+
+?>
---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	@section('head')
+	<meta charset="UTF-8">
+	<title>Laravel PHP Framework</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	@show
+</head>
+
+<body>
+	
+	<h1>The Home page</h1>
+	@yield('content')
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+</body>
+</html>
